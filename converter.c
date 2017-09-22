907db113a84277cbde3d032df48a16a381bf8f25
Francois Oligny-Lemieux <flemieux@haivision.com>
2017-09-22 12:58:25 -0400

    tiny adjustments

--- converter.c
+++ converter.c
@@ -133,6 +133,8 @@ static int fileEntryCallback(const char *name, const FILE_ENTRY *entry, void * o
 static void PrintRecursive(treeItem_T * item, int level, char * szLargeBuffer, unsigned int szLargeBufferSize)
 {
     int count = 0;
+    char * name;
+    int ret;
     //uint64_t childSize = 0;
 
     while (item)
@@ -183,74 +185,125 @@ static void PrintRecursive(treeItem_T * item, int level, char * szLargeBuffer, u
 
                 printf("]\n");
             }
+        
+            name = flexget(&item->name);
 
-            fputs(",\"size\":\"", stdout);            
+            fputs(",\"size\":\"", stdout);
             snprintf(szLargeBuffer, 100, "%"PRIu64, node->size);
             fputs(szLargeBuffer, stdout);
             fputs("\"", stdout);
-
             
-            fputs("}\n", stdout);
-            
-            if (node->size > 1024)
+            if (C_strendstr(name, ".ori"))
             {
-				char * name = flexget(&item->name);
-              FILE *tsCheck = fopen(name, "rb");
-              if (tsCheck)
-              {
-                int ret, status;
-                unsigned char buffer[188*2];
-                char output_filename[1024];
-                memset(buffer,0,sizeof(buffer));
-                size_t size_read = fread(buffer,1,sizeof(buffer),tsCheck);
-                if (buffer[0] == 0x47 && buffer[188] == 0x47)
-                {
-                   printf("YES ts file\n");
-                   
-				   char command[1024] = "";
-#if defined(_MSC_VER) || defined(WIN32)
-				   snprintf(command, sizeof(command) - 1, "C:\\ffmpeg\\bin\\ffmpeg.exe -t 30  -i \"%s\" out.wav -loglevel 56 -y > /dev/null", name); 
-#else
-				   snprintf(command, sizeof(command) - 1, "ffmpeg -t 30  -i \"%s\" out.wav -loglevel 56 -y > /dev/null", name); 
-#endif
-				   // -t 30 to limit to 30 sec worth of input
-				   // - y force yes overwrite
-                   char output[0xFFFF] = "";
-				   int stderrOnly = 1;
-				   ret = C_System2(command, output, sizeof(output), &status, stderrOnly);
-//				   
-                   if (strstr(output, "PES packet size mismatch"))
+              fputs(",\"status\":\"original, keeping untouched\"", stdout);
+            }
+            else
+            {
+               // will probe 
+               if (node->size > 1024)
+               {
+                 FILE *tsCheck = fopen(name, "rb");
+                 if (tsCheck)
+                 {
+                   int status;
+                   unsigned char buffer[188*2];
+                   char output_filename[CONVERTER_MAX_PATH];
+                   char tmp_filename[CONVERTER_MAX_PATH];
+                   memset(buffer,0,sizeof(buffer));
+                   size_t size_read = fread(buffer,1,sizeof(buffer), tsCheck);
+                   if (buffer[0] == 0x47 && buffer[188] == 0x47)
                    {
-                      printf("YES broken\n");
-	  			      uint64_t nUserAvailable = 0, nRootAvailable = 0, nTotalCapacity = 0;
-                      ret = getDiskSpace(g_directory, &nUserAvailable, &nRootAvailable, &nTotalCapacity);
-                      printf("free space %"PRIu64"\n",nUserAvailable);
-                      stderrOnly = 0;
-				   strcpy(output_filename, name);
-				   strcat(output_filename, "-fixed.ts");
-                      snprintf(command, sizeof(command) - 1, "./tsmuxer -i \"%s\" -o \"%s\" --legacy-audio true", name, output_filename); 
-                      ret = C_System2(command, output, sizeof(output), &status, stderrOnly);
-                      if (ret & status == 0)
+                      printf("YES ts file\n");
+                      
+                      char command[1024+CONVERTER_MAX_PATH] = "";
+#if defined(_MSC_VER) || defined(WIN32)
+                      snprintf(command, sizeof(command) - 1, "C:\\ffmpeg\\bin\\ffmpeg.exe -t 30  -i \"%s\" test.wav -loglevel 56 -y > /dev/null", name); 
+#else
+                      snprintf(command, sizeof(command) - 1, "./ffmpeg -t 30  -i \"%s\" test.wav -loglevel 56 -y > /dev/null", name); 
+#endif
+                      // -t 30 to limit to 30 sec worth of input
+                      // - y force yes overwrite
+                      unsigned int output_size = 0xFFFF;
+                      char * output = malloc(output_size*sizeof(char));
+                      output[0] = output[output_size-1] = '\0';
+                      int stderrOnly = 1;
+                      printf("executing cmd %s\n", command);
+                      ret = C_System2(command, output, output_size, &status, stderrOnly);
+   //               
+                      if (status)
                       {
-                         printf("Success converting\n");
+                         printf("cmd status %d\n", status);
+                      }
+                      printf("cmd %s\n", output);
+                      if (strstr(output, "PES packet size mismatch"))
+                      {
+                         printf("YES broken\n");
+                         
+                         uint64_t nUserAvailable = 0, nRootAvailable = 0, nTotalCapacity = 0;
+                         ret = getDiskSpace(g_directory, &nUserAvailable, &nRootAvailable, &nTotalCapacity);
+                         printf("free space %"PRIu64"\n",nUserAvailable);
+                         
+                         strcpy(output_filename, name);
+                         strcat(output_filename, "-fixed.ts");
+                         strcpy(tmp_filename, name);
+                         strcat(tmp_filename, ".ori");
+                         snprintf(command, sizeof(command) - 1, "./tsmuxer -i \"%s\" -o \"%s\" --legacy-audio true", name, output_filename); 
+                         printf("command %s\n", command);
+                         
+                         stderrOnly = 0;
+                         ret = C_System2(command, output, output_size, &status, stderrOnly);
+                         if (ret & status == 0)
+                         {
+                            printf("strlen of output %d, or 0x%X\n", strlen(output), strlen(output));
+                            int outputStringMaxedOut = output_size -1 == strlen(output);
+                            if (strstr(output, "Completed without errors") || outputStringMaxedOut)
+                            {
+                              printf("Success converting\n");
+                              printf("outputStringMaxedOut %d\n", outputStringMaxedOut);
+                              ret = C_MoveFileEx(name, tmp_filename, TOOLBOX_OVERWRITE_DESTINATION);
+                              if (ret >= 1)
+                              {
+                                ret = C_MoveFileEx(output_filename, name, 0);
+                                if (ret >= 1)
+                                {
+                                   printf("Success replacing files\n");
+                                   printf(",\"status\":\"success converting\"", status);
+                                }
+                                else
+                                {  // error
+                                  ret = C_MoveFileEx(tmp_filename, name, TOOLBOX_OVERWRITE_DESTINATION); // restore
+                                  printf("Restoring after error\n");
+                                }
+                              }
+                            }
+                            else
+                            {
+                              printf("There might have been a problem during convertion, output: %s\n", output);
+                              printf(",\"status\":\"error converting, see converter.log for details\"");
+                            }
+                         }
+                         else
+                         {
+                            printf("Error converting status = %d\n", status);
+                            printf("\noutput:%s\n", output);
+                            printf(",\"status\":\"error converting, status %d\"", status);
+                         }  
                       }
                       else
                       {
-                         printf("Error converting status = %d\n", status);
-                         printf("\noutput:%s\n", output);
-                      }  
+                         printf("not broken\n");
+                         fputs(",\"status\":\"TS file not broken, keeping untouched\"", stdout);
+                      }
                    }
-				   else
-				   {
-					   printf("not broken\n");
-				   }
-                }
-                else
-                {
-                   printf("NO ts file\n");
-                }
-              }
+                   else
+                   {
+                      printf("NOT ts file\n");
+                   }
+                   fclose(tsCheck);
+                 }
+               }
             }
+            fputs("}\n", stdout);            
             
             count++;
         }
